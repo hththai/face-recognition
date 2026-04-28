59db6bc22994dc40bebd0a6467b03f7a616ac4a3
hththai <huythaicmvn@gmail.com>
2026-04-28 22:41:50 +1000

    refactor: change folder name

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	convertservice "myproject/convert"
 	"os"
-
-	"myproject/service"
 )
 
 func main() {
-	if err := service.ConvertFolder("original-images", "convert-folder"); err != nil {
+	if err := convertservice.ConvertFolder("original-images", "convert-folder"); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
